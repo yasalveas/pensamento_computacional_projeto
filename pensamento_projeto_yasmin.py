476269ab6ce1c278364c561b6f2ac42bdba02a80
Aluno em Vocação Nicolas santana <yasilvalves26@gmail.com>
2026-07-02 18:10:30 -0300

    update_projeto

---
+++ pensamento_projeto_yasmin.py
@@ -0,0 +1,41 @@
+'''
+>>projeto açaiteria:                                                              
+
+
+
+
+ 
+
+'''
+
+
+
+#Ufa,quebrei a maldição
+# print('Ola, Mundo') 
+
+print('-'* 48 + '\n' )
+print('Bem vindo ao sistema de vendas - açaiteria!\n')
+print('1 - cadastrar produto')
+print('2 - Listar produtos')
+print('3 - Realizar venda')
+print("4 -  Combos de açai")
+print("5 - Bebidas ")
+print("6 - Sobre nós")
+print("7 - Fale conosco")
+print("8 - Envie seu Feedback")
+print("9 - Recomendaçoes")
+print("0 - Sair do sistema")
+print('\n----------------------------\n')
+
+opçao__definida = int(input("digite a opçao desejada:"))
+if opcao__definida == 1:
+   print(Cadastrando produto...'') 
+elif opçao__definida == 2:
+print (listando produto)
+elif opçao__definida == 3:
+print("realizando venda...")
+elif opçao__definida == 0:
+print ("combos de açai ")
+
+
+
